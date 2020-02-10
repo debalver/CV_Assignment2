0baebce19d263d206a082b18f07fc0585c73a9fb
Quentin Devaud <quentin.devaud@epfl.ch>
2020-02-10 15:57:25 +0100

    Andres implementation of task 2

--- task2.py
+++ task2.py
@@ -2,7 +2,9 @@ import numpy as np
 import utils
 import matplotlib.pyplot as plt
 import typing
-from task2a import cross_entropy_loss, SoftmaxModel, one_hot_encode, pre_process_images
+import time
+from tqdm import tqdm
+from task2a import cross_entropy_loss, SoftmaxModel, one_hot_encode, pre_process_images, find_mean_and_deviation
 np.random.seed(0)
 
 
@@ -16,7 +18,17 @@ def calculate_accuracy(X: np.ndarray, targets: np.ndarray,
     Returns:
         Accuracy (float)
     """
-    accuracy = 0
+    # Compute the outputs
+    outputs = model.forward(X)
+    # Counting the correct predictions
+    nb_predictions = outputs.shape[0]
+    nb_correct_predictions = 0
+    for row, output in enumerate(outputs):
+        index = np.argmax(output)
+        if targets[row][index] == 1:
+            nb_correct_predictions += 1
+
+    accuracy = nb_correct_predictions / nb_predictions
     return accuracy
 
 
@@ -40,22 +52,62 @@ def train(
     val_loss = {}
     train_accuracy = {}
     val_accuracy = {}
+    # Early stop variables
+    early_stopped_weight_j = np.zeros((model.ws[0].shape[0], model.ws[0].shape[1]))
+    early_stopped_weight_k = np.zeros((model.ws[1].shape[0], model.ws[1].shape[1]))
+    early_stop_counter = 0
+    best_loss = float("inf")
 
     global_step = 0
-    for epoch in range(num_epochs):
+    for epoch in tqdm(range(num_epochs)):
         for step in range(num_batches_per_epoch):
             start = step * batch_size
             end = start + batch_size
             X_batch, Y_batch = X_train[start:end], Y_train[start:end]
 
+            outputs = model.forward(X_batch)
+            model.backward(X_batch, outputs, Y_batch)
+            # Update the weights
+            model.ws[0] = model.ws[0] - learning_rate * model.grads[0]
+            model.ws[1] = model.ws[1] - learning_rate * model.grads[1]
+
+            # Track training loss continuously over the entire X_Train and not only the current batch
+            #outputs_training = model.forward(X_train)
+            #_train_loss = cross_entropy_loss(Y_batch, outputs)
+            #train_loss[global_step] = _train_loss
+
             # Track train / validation loss / accuracy
             # every time we progress 20% through the dataset
             if (global_step % num_steps_per_val) == 0:
-                _val_loss = 0
+                # Test the validation data on the network
+                outputs_validation = model.forward(X_val)
+                _val_loss = cross_entropy_loss(Y_val, outputs_validation)
                 val_loss[global_step] = _val_loss
 
-                _train_loss = 0
+                # Track training loss over the entire X_Train and not only the current batch
+                # once every validation epoch
+                outputs_training = model.forward(X_train)
+                _train_loss = cross_entropy_loss(Y_train, outputs_training)
                 train_loss[global_step] = _train_loss
+
+                # Early stop implementation
+
+                # If the loss does not reduce compared to best loss, increment counter
+                # Otherwise, set the counter to 0 and update best loss
+                if _val_loss >= best_loss:
+                    early_stop_counter += 1
+                else:
+                    early_stop_counter = 0
+                    best_loss = _val_loss
+                    early_stopped_weight_j = model.ws[0]
+                    early_stopped_weight_k = model.ws[1]
+                # If 30 times in a row a new best loss was not achieved, stop the program
+                if early_stop_counter == 30:
+                    print("The cross entropy loss for validation data increased too much, thus triggering "
+                          "the early stop at step : " + str(global_step) + " and epoch : " + str(epoch))
+                    model.ws[0] = early_stopped_weight_j
+                    model.ws[1] = early_stopped_weight_k
+                    return model, train_loss, val_loss, train_accuracy, val_accuracy
 
                 train_accuracy[global_step] = calculate_accuracy(
                     X_train, Y_train, model)
@@ -67,10 +119,24 @@ def train(
 
 
 if __name__ == "__main__":
+
+    # Measure execution time
+    start = time.time()
+
     # Load dataset
     validation_percentage = 0.2
     X_train, Y_train, X_val, Y_val, X_test, Y_test = utils.load_full_mnist(
         validation_percentage)
+
+    # Preprocess and adapt the data
+    mean, standard_deviation = find_mean_and_deviation(X_train)
+    X_train = pre_process_images(X_train, mean, standard_deviation)
+    X_val = pre_process_images(X_val, mean, standard_deviation)
+    X_test = pre_process_images(X_test, mean, standard_deviation)
+
+    Y_train = one_hot_encode(Y_train, 10)
+    Y_val = one_hot_encode(Y_val, 10)
+    Y_test = one_hot_encode(Y_test, 10)
 
     # Hyperparameters
     num_epochs = 20
@@ -89,6 +155,11 @@ if __name__ == "__main__":
         neurons_per_layer,
         use_improved_sigmoid,
         use_improved_weight_init)
+
+    # Initializing weights before any training
+    model.ws[0] = np.random.uniform(-1, 1, (785, 64))
+    model.ws[1] = np.random.uniform(-1, 1, (64, 10))
+
     model, train_loss, val_loss, train_accuracy, val_accuracy = train(
         model,
         [X_train, Y_train, X_val, Y_val, X_test, Y_test],
@@ -113,22 +184,33 @@ if __name__ == "__main__":
     print("Final Test accuracy:",
           calculate_accuracy(X_test, Y_test, model))
 
+    # Execution time calculation
+    end = time.time()
+    time_in_seconds = end - start
+    if (time_in_seconds > 60):
+        print("The process took: " + str(int(time_in_seconds / 60)) + "min " + str(int(time_in_seconds % 60)) + "s")
+    else:
+        print("The process took: " + str(int(time_in_seconds)) + "s")
+
+
     # Plot loss
-    plt.figure(figsize=(20, 8))
-    plt.subplot(1, 2, 1)
-    plt.ylim([0.1, .5])
+    plt.ylim([0, 0.5])
     utils.plot_loss(train_loss, "Training Loss")
     utils.plot_loss(val_loss, "Validation Loss")
-    plt.xlabel("Number of gradient steps")
+    plt.xlabel('Number of gradient steps')
     plt.ylabel("Cross Entropy Loss")
+    plt.title('Printout of Cross Entropy Loss')
     plt.legend()
-    plt.subplot(1, 2, 2)
+    plt.savefig("images/task_2_softmax_loss.png", dpi=200)
+    plt.show()
+
     # Plot accuracy
-    plt.ylim([0.9, 1.0])
+    plt.ylim([0.8, 0.99])
     utils.plot_loss(train_accuracy, "Training Accuracy")
     utils.plot_loss(val_accuracy, "Validation Accuracy")
-    plt.legend()
-    plt.xlabel("Number of gradient steps")
+    plt.xlabel('Number of gradient steps')
     plt.ylabel("Accuracy")
-    plt.savefig("softmax_train_graph.png")
+    plt.title('Printout of accuracy')
+    plt.legend()
+    plt.savefig("images/task_2_softmax_accuracy.png", dpi=200)
     plt.show()
--- task2a.py
+++ task2a.py
@@ -1,22 +1,45 @@
 import numpy as np
 import utils
 import typing
+from tqdm import tqdm
 np.random.seed(1)
 
+def find_mean_and_deviation(X: np.ndarray):
+    """
+    Args:
+        X: images of shape [training set size, 784] in the range (0, 255)
+    Returns:
+        mean: mean of the training set
+        standard_deviation: standard deviation of the training set
+    """
+    assert X.shape[1] == 784, \
+        f"X.shape[1]: {X.shape[1]}, should be 784"
+    # Calculates the mean and standard deviation from the whole training set
+    mean = np.sum(X) / (X.shape[0] * X.shape[1])
+    variance = np.sum((X - mean) ** 2) / (X.shape[0] * X.shape[1])
+    standard_deviation = np.sqrt(variance)
 
-def pre_process_images(X: np.ndarray):
+    return mean, standard_deviation
+
+def pre_process_images(X: np.ndarray, mean, standard_deviation):
     """
     Args:
         X: images of shape [batch size, 784] in the range (0, 255)
+        mean: mean of the training set
+        standard_deviation: standard deviation of the training set
     Returns:
         X: images of shape [batch size, 785]
     """
-    print("Hey, that's a test for GitHub")
-    print("Commit from PyCharm")
-    print("Second commit from PyCharm")
     assert X.shape[1] == 784,\
         f"X.shape[1]: {X.shape[1]}, should be 784"
-    return X
+    # Normalizes the input X
+    X_norm = (X - mean) / standard_deviation
+
+    # Applies the bias trick by adding a column of 1 in the front
+    bias_trick = np.ones((np.shape(X)[0], 1))
+    X_norm = np.hstack((bias_trick, X_norm))
+
+    return X_norm
 
 
 def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray):
@@ -29,6 +52,8 @@ def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray):
     """
     assert targets.shape == outputs.shape,\
         f"Targets shape: {targets.shape}, outputs: {outputs.shape}"
+    ce = targets * np.log(outputs)
+    return np.sum(ce) / (-1 * ce.shape[0])
     raise NotImplementedError
 
 
@@ -41,13 +66,17 @@ class SoftmaxModel:
                  use_improved_weight_init: bool  # Task 3c hyperparameter
                  ):
         # Define number of input nodes
-        self.I = None
+        self.I = 785 # 28x28=784 pixels + 1 for bias
         self.use_improved_sigmoid = use_improved_sigmoid
 
         # Define number of output nodes
         # neurons_per_layer = [64, 10] indicates that we will have two layers:
         # A hidden layer with 64 neurons and a output layer with 10 neurons.
         self.neurons_per_layer = neurons_per_layer
+
+        # Values computed en forward function and needed in backward function
+        self.aj = None
+        self.zj = None
 
         # Initialize the weights
         self.ws = []
@@ -67,6 +96,18 @@ class SoftmaxModel:
         Returns:
             y: output of model with shape [batch size, num_outputs]
         """
+        # Sigmoid function for the hidden layer
+        #hidden_layer_activation = np.zeros((self.ws[0].shape[0], self.ws[0].shape[1]))
+        self.zj = X @ self.ws[0]
+        self.aj = 1 / (1 + np.exp(-self.zj)) # Hidden layer activation
+
+        # Softmax function for the output layer
+        y = np.exp(self.aj @ self.ws[1])
+        # Normalize each y^n by the sum of the vector's values
+        sum = np.sum(y, axis=1)
+        output_layer_activation = y/sum[:, None]
+
+        return output_layer_activation
         return None
 
     def backward(self, X: np.ndarray, outputs: np.ndarray,
@@ -81,7 +122,20 @@ class SoftmaxModel:
             f"Output shape: {outputs.shape}, targets: {targets.shape}"
         # A list of gradients.
         # For example, self.grads[0] will be the gradient for the first hidden layer
-        self.grads = []
+        # self.grads = []
+
+        # Output layer backpropagation
+        delta_k = (targets - outputs) / (-X.shape[0])
+        self.grads[1] = self.aj.T @ delta_k
+
+        # Hidden layer backpropagation
+        exp = np.exp(-self.zj)
+        sigmoid_derivative = exp / ((1 + exp) ** 2)
+        #print("delta_k.shape = ", delta_k.shape, "self.aj.shape = ", self.aj.shape, "self.zj.shape = ", self.zj.shape, " sigmoid_derivative.shape = ", sigmoid_derivative.shape, "ws.[1].shape = ", self.ws[1].shape)
+
+        delta_j = (delta_k @ self.ws[1].T) * sigmoid_derivative
+        #print("delta_j.shape = ", delta_j.shape, " X.shape = ", X.shape)
+        self.grads[0] = X.T @ delta_j
 
         for grad, w in zip(self.grads, self.ws):
             assert grad.shape == w.shape,\
@@ -99,18 +153,26 @@ def one_hot_encode(Y: np.ndarray, num_classes: int):
     Returns:
         Y: shape [Num examples, num classes]
     """
+    aux_array = np.zeros((np.shape(Y)[0],num_classes - 1), dtype=int)
+    Y = np.hstack((Y, aux_array))
+    # For each row it sets array[row][0] to 0 and then writes 1 on the previous array[row][0] value
+    for i in range(np.shape(Y)[0]):
+        value = Y[i,0]
+        Y[i,0] = 0
+        Y[i,value] = 1
+    return Y
     raise NotImplementedError
 
 
 def gradient_approximation_test(
         model: SoftmaxModel, X: np.ndarray, Y: np.ndarray):
     """
-        Numerical approximation for gradients. Should not be edited. 
+        Numerical approximation for gradients. Should not be edited.
         Details about this test is given in the appendix in the assignment.
     """
     epsilon = 1e-3
     for layer_idx, w in enumerate(model.ws):
-        for i in range(w.shape[0]):
+        for i in tqdm(range(w.shape[0])):
             for j in range(w.shape[1]):
                 orig = model.ws[layer_idx][i, j].copy()
                 model.ws[layer_idx][i, j] = orig + epsilon
@@ -128,6 +190,7 @@ def gradient_approximation_test(
                     model.grads[layer_idx][i, j]
                 assert abs(difference) <= epsilon**2,\
                     f"Calculated gradient is incorrect. " \
+                    f"Layer IDX = {layer_idx}, i={i}, j={j}.\n" \
                     f"Approximation: {gradient_approximation}, actual gradient: {model.grads[layer_idx][i, j]}\n" \
                     f"If this test fails there could be errors in your cross entropy loss function, " \
                     f"forward function or backward function"
@@ -142,7 +205,10 @@ if __name__ == "__main__":
         f"Expected the vector to be [0,0,0,1,0,0,0,0,0,0], but got {Y}"
 
     X_train, Y_train, *_ = utils.load_full_mnist(0.1)
-    X_train = pre_process_images(X_train)
+    mean, standard_deviation = find_mean_and_deviation(X_train)
+    #print("mean: ", mean, " / standard deviation: ", standard_deviation)
+    #exit()
+    X_train = pre_process_images(X_train, mean, standard_deviation)
     Y_train = one_hot_encode(Y_train, 10)
     assert X_train.shape[1] == 785,\
         f"Expected X_train to have 785 elements per image. Shape was: {X_train.shape}"
@@ -152,16 +218,20 @@ if __name__ == "__main__":
     use_improved_weight_init = False
     model = SoftmaxModel(
         neurons_per_layer, use_improved_sigmoid, use_improved_weight_init)
-    logits = model.forward(X_train)
+    """logits = model.forward(X_train)
     np.testing.assert_almost_equal(
         logits.mean(), 1/10,
-        err_msg="Since the weights are all 0's, the softmax activation should be 1/10")
+        err_msg="Since the weights are all 0's, the softmax activation should be 1/10")"""
 
     # Gradient approximation check for 100 images
+    print("X_train.shape = ", X_train.shape)
     X_train = X_train[:100]
+    print("X_train.shape = ", X_train.shape)
     Y_train = Y_train[:100]
-    for i in range(2):
-        if i != 0:
-            gradient_approximation_test(model, X_train, Y_train)
-        model.ws = [np.random.randn(*w.shape) for w in model.ws]
-#        model.w = np.random.randn(*model.w.shape)
+
+    for layer_idx, w in enumerate(model.ws):
+        model.ws[layer_idx] = np.random.uniform(-1, 1, size=w.shape)
+
+    print("Llamando a grandient_approximation_test...")
+    gradient_approximation_test(model, X_train, Y_train)
+    print("gradient approximation test ya se ha ejecutado, todo bien :)")
